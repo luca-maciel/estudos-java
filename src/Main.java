036ba8f1419822b0414718fb010e3711c9ceb56c
Lucas Maciel <lalucasforte56@gmail.com>
2025-08-08 11:21:32 -0300

    add member function are added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 class People{
     String name;
@@ -25,7 +24,7 @@ class Group{
         }
         else{
             for(People member : this.members){
-                System.out.println(member);
+                System.out.println(member.name);
             }
         }
 
@@ -35,13 +34,16 @@ class Group{
         this.members.add(newMember);
     }
 
+    void removeMember(People member){
+        this.members.remove(member);
+    }
+
 }
 
 public class Main {
     static Scanner input = new Scanner(System.in);
     public static void Menu(){
-
-        System.out.println("Hello! What you wanna do?\n[1] - See all group members");
+        System.out.println("Hello! What you wanna do?\n[1] - See all group members\n[2] - Add member to group\n[3] - Remove member from group");
     }
 
     public static void main(String[] args) {
@@ -50,8 +52,23 @@ public class Main {
         while (chose != 0){
             Main.Menu();
             chose = input.nextInt();
-            if (chose == 1){
-                testGroup.showMembers();
+//            if (chose == 1){
+//                testGroup.showMembers();
+//            }
+            switch (chose){
+                case 1:
+                    testGroup.showMembers();
+                    break;
+                case 2:
+                    input.nextLine();
+                    System.out.print("Enter the name of the new member: ");
+                    String name = input.nextLine();
+                    System.out.print("Enter the new member age: ");
+                    int age = input.nextInt();
+                    input.nextLine();
+                    People newMember = new People(name, age);
+                    testGroup.addMember(newMember);
+                    break;
             }
         }
 
